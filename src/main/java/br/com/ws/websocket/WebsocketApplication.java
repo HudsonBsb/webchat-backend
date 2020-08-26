2cc564d4f34dc1485e54fca57d24635438bb442a
Hudson Henrique Queiroz Gomes <hudson.gomes@originalapp.com.br>
2020-08-26 13:08:58 -0300

    config war

--- src/main/java/br/com/ws/websocket/WebsocketApplication.java
+++ src/main/java/br/com/ws/websocket/WebsocketApplication.java
@@ -2,9 +2,10 @@ package br.com.ws.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebsocketApplication {
+public class WebsocketApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
